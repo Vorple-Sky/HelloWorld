dd5673360054558394dc3cd9b5c14c530cc12625
Vorple-Sky <hannahg3211@gmail.com>
2022-01-27 15:24:06 -0800

    First Commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(1000, 800);
+
+  textSize(48);
+  textAlign(CENTER);
+
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+
+  fill(225,0,100);
+  text('Hello, World', width/2, height/2);
+  text('Hello, USF', width/2, height/2 + 100);
+
+}
